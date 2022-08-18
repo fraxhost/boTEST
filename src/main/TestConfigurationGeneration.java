49e969cb8091c54de05ffd9bd7ffcba1ac80f7b0
ahmedryan <bsse1011@iit.du.ac.bd>
2022-08-18 22:09:37 +0600

    updated

--- src/main/TestConfigurationGeneration.java
+++ src/main/TestConfigurationGeneration.java
@@ -24,61 +24,61 @@ public class TestConfigurationGeneration {
         System.out.println(testConfigurationObject);
         System.out.println(testCases);
 
-        // TODO: print / remove later
-        try {
-            try {
-                File myObj = new File("F:\\output\\configuration.txt");
-                if (myObj.createNewFile()) {
-                    System.out.println("File created: " + myObj.getName());
-                } else {
-                    System.out.println("File already exists.");
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-
-
-            FileWriter myWriter = new FileWriter("F:\\output\\configuration.txt", true);
-
-            try {
-                myWriter.write(testConfigurationObject + "\n");
-                System.out.println("Successfully wrote to the file.");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            myWriter.close();
-        } catch (IOException ex) {
-            System.out.println("File Error: " + ex);
-        }
-
-        // TODO: print / remove later
-        try {
-            try {
-                File myObj = new File("F:\\output\\testcases.txt");
-                if (myObj.createNewFile()) {
-                    System.out.println("File created: " + myObj.getName());
-                } else {
-                    System.out.println("File already exists.");
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-
-
-            FileWriter myWriter = new FileWriter("F:\\output\\testcases.txt", true);
-
-            try {
-                myWriter.write(testCases + "\n");
-                System.out.println("Successfully wrote to the file.");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            myWriter.close();
-        } catch (IOException ex) {
-            System.out.println("File Error: " + ex);
-        }
+//        // TODO: print / remove later
+//        try {
+//            try {
+//                File myObj = new File("F:\\output\\configuration.txt");
+//                if (myObj.createNewFile()) {
+//                    System.out.println("File created: " + myObj.getName());
+//                } else {
+//                    System.out.println("File already exists.");
+//                }
+//            } catch (IOException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace();
+//            }
+//
+//
+//            FileWriter myWriter = new FileWriter("F:\\output\\configuration.txt", true);
+//
+//            try {
+//                myWriter.write(testConfigurationObject + "\n");
+//                System.out.println("Successfully wrote to the file.");
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//            myWriter.close();
+//        } catch (IOException ex) {
+//            System.out.println("File Error: " + ex);
+//        }
+//
+//        // TODO: print / remove later
+//        try {
+//            try {
+//                File myObj = new File("F:\\output\\testcases.txt");
+//                if (myObj.createNewFile()) {
+//                    System.out.println("File created: " + myObj.getName());
+//                } else {
+//                    System.out.println("File already exists.");
+//                }
+//            } catch (IOException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace();
+//            }
+//
+//
+//            FileWriter myWriter = new FileWriter("F:\\output\\testcases.txt", true);
+//
+//            try {
+//                myWriter.write(testCases + "\n");
+//                System.out.println("Successfully wrote to the file.");
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//            myWriter.close();
+//        } catch (IOException ex) {
+//            System.out.println("File Error: " + ex);
+//        }
 //        System.out.println(openApiSpecification.getSpecification());
     }
 }
